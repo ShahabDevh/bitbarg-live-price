0ab4f653118a0fa69383345ff099bc945f0b95c1
shahab hosseini <shahab.devh@gmail.com>
2022-04-22 04:18:28 +0430

    Added pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
+import rtlPlugin from 'stylis-plugin-rtl';
+import createCache from '@emotion/cache';
+import { CacheProvider, ThemeProvider } from '@emotion/react';
+import { Provider } from 'react-redux';
+import { prefixer } from 'stylis';
+import { wrapper, store } from '../store/storeConfig';
+import { theme } from '../utils/muiTheme';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+
+const queryClient = new QueryClient();
+
+// Create rtl cache
+const cacheRtl = createCache({
+	key: 'muirtl',
+	stylisPlugins: [prefixer, rtlPlugin],
+});
+
+function RTL(props) {
+	return <CacheProvider value={cacheRtl}>{props.children}</CacheProvider>;
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+	return (
+		<Provider store={store}>
+			<QueryClientProvider client={queryClient}>
+				<RTL>
+					<ThemeProvider theme={theme}>
+						<Component {...pageProps} />
+					</ThemeProvider>
+				</RTL>
+				{process.env.NODE_ENV === 'development' && <ReactQueryDevtools />}
+			</QueryClientProvider>
+		</Provider>
+	);
+}
+
+export default wrapper.withRedux(MyApp);
---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+	return (
+		<Html lang="fa">
+			<Head>
+				<link
+					href="fonts/IRANSans/iransans.css"
+					rel="preload"
+					as="font"
+					crossOrigin=""
+				/>
+			</Head>
+			<body dir="rtl">
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
